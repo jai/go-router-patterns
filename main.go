23456a644fe98c74c8ca8e614125c05fc1355fe2
Jai Govindani <govindani@gmail.com>
2019-07-13 15:30:49 +0700

    Initial example using Gin
    Signed-off-by: Jai Govindani <govindani@gmail.com>

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Using Gin
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
+}
